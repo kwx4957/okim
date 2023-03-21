5cf590d576bba8a51c4772059f71128d44fbea95
dansam <33277725+kwx4957@users.noreply.github.com>
2023-03-21 11:12:37 +0900

    Feat: User 조희 기능
    User 조희 기능 추가
    
    Related to: #15

--- src/main/java/com/goorm/okim/presentation/UserController.java
+++ src/main/java/com/goorm/okim/presentation/UserController.java
@@ -1,9 +1,25 @@
 package com.goorm.okim.presentation;
 
-import org.springframework.web.bind.annotation.*;
+import com.goorm.okim.common.Response;
+import com.goorm.okim.infra.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable("userId") long userId){
+        return Response.success(userRepository.findById(userId));
+    }
 }
