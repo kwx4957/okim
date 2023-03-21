65188317a426ab75db8abbdf8681dfd0042b01f6
dansam <33277725+kwx4957@users.noreply.github.com>
2023-03-21 16:25:03 +0900

    Fix: Organization @getter 추가
    1. Organization @getter 추가
    
    Related to: #16

--- src/main/java/com/goorm/okim/domain/Organization.java
+++ src/main/java/com/goorm/okim/domain/Organization.java
@@ -2,8 +2,10 @@ package com.goorm.okim.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Organization {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
