18c3ad5077bcce2783831d9c68c595e5b740c4cd
dansam <33277725+kwx4957@users.noreply.github.com>
2023-03-22 10:47:14 +0900

    Feat: User email 조회 추가
    1. User Controller api 추가
    2. User repository 함수 추가
    3. User Service 이메일 존재 기능 추가
    4. User Service 이메일 유효성 검사 추가
    
    Related to:	#21

--- src/main/java/com/goorm/okim/presentation/UserController.java
+++ src/main/java/com/goorm/okim/presentation/UserController.java
@@ -1,9 +1,6 @@
 package com.goorm.okim.presentation;
 
-import com.goorm.okim.common.Response;
-import com.goorm.okim.infra.repository.UserRepository;
 import com.goorm.okim.service.UserService;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<?> getUserTask(@PathVariable("userId") long userId){
         return userService.getUserTask(userId);
+    }
+
+    @GetMapping("/email/{email}/check")
+    public ResponseEntity<?> existEmail(@PathVariable("email") String email){
+        return userService.existEmail(email);
     }
 }
