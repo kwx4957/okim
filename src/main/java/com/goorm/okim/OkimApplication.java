a3120d5ebc348a9f7464298c48d7ebdcb06d52bc
kmss6905 <kmss69052@naver.com>
2023-04-11 21:14:16 +0900

    edit timezone

--- src/main/java/com/goorm/okim/OkimApplication.java
+++ src/main/java/com/goorm/okim/OkimApplication.java
@@ -1,12 +1,20 @@
 package com.goorm.okim;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class OkimApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OkimApplication.class, args);
--- src/main/java/com/goorm/okim/common/BaseEntity.java
+++ src/main/java/com/goorm/okim/common/BaseEntity.java
@@ -1,23 +1,30 @@
 package com.goorm.okim.common;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
-    @Column(updatable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
+public abstract class BaseEntity {
 
-    @LastModifiedDate
-    private LocalDateTime lastModifiedAt;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime lastModifiedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.lastModifiedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.lastModifiedAt = ZonedDateTime.now();
+    }
+
 }
